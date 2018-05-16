0d6203223ad339c14f93fef4ddb07c92e97e0f6f
GeorgianaRosu <39053144+GeorgianaRosu@users.noreply.github.com>
2018-05-16 11:26:55 +0200

    georgy

--- trabajo_info.c
+++ trabajo_info.c
@@ -37,7 +37,7 @@ int main()
 			laberinto = (char**)malloc(fila * sizeof(char*));
 			if ('\0' == laberinto)
 			{
-			printf(stderr, "Problemas al reservar la memoria\n");
+			fprintf(stderr, "Problemas al reservar la memoria\n");
 			return(-1);
 			for ( en = 0; en < fila; en++)
 				{
@@ -58,7 +58,138 @@ int main()
 			fclose(pf);
 			break;
 			}
+	}    
+			
+		case 2:
+		{
+			random=rand()%4;
+			switch (random)
+			{
+				case 0:
+					{
+						fila=0;
+						columna=0;
+						pf=open("Labmed1.txt", "r"); //Hay que crearlo
+						fscanf(pf, "%d", &fila);
+						fscanf(pf, "%d", &columna);
+						fgetc(pf);
+						//Se crea en la memoria el laberinto reservando la memoria
+						laberinto=(char**)malloc(fila*sizeof(char*));
+						
+						if ('\0' == laberinto)
+						{
+							fprintf(stderr, "Problemas al reservar la memoria.\n");
+							return (-1);
+						}
+						//Se crea en la memoria cada una de las posiciones del array
+						
+						for (en=0; en<fila; en++)
+						{
+							laberinto[en]= (char*)malloc((columna+1)*sizeof(char));
+						    if ('\0'==laberinto[en])
+						    {
+						    	fprintf(stderr, "Problemas al reservar la memoria\n");
+						    	return(-1);
+							}
+							//Para poder ser liberado
+							laberinto[en][columna] = '\0';
+						}
+						
+						for (i=0; i<fila; i++)
+						{
+							fgets(laberinto[i], columna+1, pf);
+							while (fgetc(pf) != '\n'); 
+						}
+						
+						fclose(pf);
+						break;
+					}   
+					
+				
+				case 1:
+					{
+						fila=0;
+						columna=0;
+						pf=open("Labmed2.txt", "r"); //Hay que crearlo
+						fscanf(pf, "%d", &fila);
+						fscanf(pf, "%d", &columna);
+						fgetc(pf);
+						//Se crea en la memoria el laberinto reservando la memoria
+						laberinto=(char**)malloc(fila*sizeof(char*));
+						
+						if ('\0' == laberinto)
+						{
+							fprintf(stderr, "Problemas al reservar la memoria.\n");
+							return (-1);
+						}
+						//Se crea en la memoria cada una de las posiciones del array
+						for (en=0; en<fila; en++)
+						{
+							laberinto[en]= (char*)malloc((columna+1)*sizeof(char));
+						    if ('\0'==laberinto[en])
+						    {
+						    	fprintf(stderr, "Problemas al reservar la memoria\n");
+						    	return(-1);
+							}
+							//Para poder ser liberado
+							laberinto[en][columna] = '\0';
+						}
+						
+						for (i=0; i<fila; i++)
+						{
+							fgets(laberinto[i], columna+1, pf);
+							while (fgetc(pf) != '\n');
+						}
+						fclose(pf);
+						break;
+						
+						}
+				
+				case 2:
+					{
+						fila=0;
+						columna=0;
+						pf=open("Labmed3.txt", "r"); //Hay que crearlo
+						fscanf(pf, "%d", &fila);
+						fscanf(pf, "%d", &columna);
+						fgetc(pf);
+						//Se crea en la memoria el laberinto reservando la memoria
+						laberinto=(char**)malloc(fila*sizeof(char*));
+						
+						if ('\0' == laberinto)
+						{
+							fprintf(stderr, "Problemas al reservar la memoria.\n");
+							return (-1);
+						}
+						//Se crea en la memoria cada una de las posiciones del array
+						for (en=0; en<fila; en++)
+						{
+							laberinto[en]= (char*)malloc((columna+1)*sizeof(char));
+						    
+						    if ('\0'==laberinto[en])
+						    {
+						    	fprintf(stderr, "Problemas al reservar la memoria\n");
+						    	return(-1);
+							}
+							//Para poder ser liberado
+							laberinto[en][columna] = '\0';
+						}
+						
+						for (i=0; i<fila; i++)
+						{
+							fgets(laberinto[i], columna+1, pf);
+							while (fgetc(pf) != '\n'); 
+							}
+						}
+						
+						fclose(pf);
+						break;
+					}
+						
+						
+					} //He quitado los errores que habían. Solo eran las llaves
+			}
+			
 		
 		}
-	}
-}
+
